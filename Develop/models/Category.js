44f697057e6ddf1eb36f8a2274fa38d99beea535
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-06-20 23:18:30 -0500

    saving work... added columns to models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    catergeryName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
