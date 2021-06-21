2021308ee408ddcece4c9df8d880aadaf177db71
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-06-21 16:57:01 -0500

    testing

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -55,22 +55,56 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   // create a new tag
   Tag.create({
-    tag_name: req.body.tag_name
+    tag_name: req.body.tag_name,
   })
-  .then(databaseTag => res.json(databaseTag))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((databaseTag) => res.json(databaseTag))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((databaseTag) => {
+      if (!databaseTag[0]) {
+        res.status(404).json({
+          message: "NOR FOUND",
+        });
+        return;
+      }
+      res.json(databaseTag);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((databaseTag) => {
+      if (!databaseTag) {
+        res.status(404).json({
+          message: "NOT FOUND",
+        });
+        return;
+      }
+      res.json(databaseTag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
